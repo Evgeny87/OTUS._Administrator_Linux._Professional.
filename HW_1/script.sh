a015b0d8c9419173e90720dc75e36482991a2f80
Evgeny87 <sun_nik_off@mail.ru>
2026-04-02 23:02:37 +0300

    Create script.sh

---
+++ HW_1/script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# Проверка версии
+uname -sr
+
+# Подготовка директории и загрузка deb-пакетов
+mkdir kernel && cd kernel
+wget https://kernel.ubuntu.com/mainline/v6.19.10/amd64/linux-headers-6.19.10-061910-generic_6.19.10-061910.202603251147_amd64.deb
+wget https://kernel.ubuntu.com/mainline/v6.19.10/amd64/linux-headers-6.19.10-061910_6.19.10-061910.202603251147_all.deb
+wget https://kernel.ubuntu.com/mainline/v6.19.10/amd64/linux-image-unsigned-6.19.10-061910-generic_6.19.10-061910.202603251147_amd64.deb
+wget https://kernel.ubuntu.com/mainline/v6.19.10/amd64/linux-modules-6.19.10-061910-generic_6.19.10-061910.202603251147_amd64.deb
+
+# Установка пакетов
+sudo dpkg -i *.deb
+
+# Обновление загрузчика
+sudo update-grub
