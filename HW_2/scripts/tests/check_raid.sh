e29945cfda23d75e78dee41c7f9cd7896bc6e979
Evgeny87 <sun_nik_off@mail.ru>
2026-04-12 20:57:58 +0000

    tests

--- HW_2/scripts/tests/check_raid.sh
+++ HW_2/scripts/tests/check_raid.sh
@@ -3,7 +3,7 @@ set -uo pipefail
 
 echo "=== INFRASTRUCTURE TESTS ==="
 grep -q "active raid10" /proc/mdstat && echo "[PASS] RAID Active" || echo "[FAIL] RAID Inactive"
-[[ $(lsblk -nlo NAME /dev/md10 | grep -c "p") -eq 5 ]] && echo "[PASS] 5 Partitions found" || echo "[FAIL] Partitions mismatch"
+[[ $(lsblk -nlo NAME /dev/md10 | grep -E 'p[0-9]+$' | wc -l) -eq 5 ]] && echo "[PASS] 5 Partitions found" || echo "[FAIL] Partitions mismatch"
 for i in {1..5}; do
     mountpoint -q "/mnt/raid10/part$i" && echo "[PASS] Part $i mounted" || echo "[FAIL] Part $i error"
 done
